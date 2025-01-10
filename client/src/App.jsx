517d72f692de44f7da5b9b64d9f349e1b6623757
Mesh296 <minhhan181@gmail.com>
2025-01-10 20:05:50 +0300

    feat: create navbar, react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,25 @@
 import { useState } from 'react'
-
+import { PrimaryButton } from './components/PrimaryButton'
+import { Navbar } from './components/Navbar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HomePage } from './pages/HomePage'
+import { AboutPage } from './pages/AboutPage'
+import { LoginPage } from './pages/LoginPage'
+import { RegisterPage } from './pages/RegisterPage'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='text-3xl font-bold stroke-stroke-1'>
-        Hello world
-      </h1>
-      <button className='px-4 py-3 bg-brand-color cursor-pointer text-brand-color-text rounded-[7px] hover:bg-on-surface-2 hover:text-brand-color border hover:border-stroke-light transition-all duration-200'>
-        Get started
-      </button>
-      <p>con cac du ma may
-      </p>
+      <Router>
+        <Navbar />
+        <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </Router>
     </>
   )
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,24 +1,63 @@
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
   ],
   theme: {
-    
+
     extend: {
       colors: {
+        // Độ sáng Light mode: On surface 1 > Surface ≥ On surface 2.
+        // Độ sáng Dark mode: On surface 2 > On surface 1 > Surface.
+        // stroke light + surface
+        // stroke bold + on-surface-1
+        // Element Primary: color for Heading, Title, selected and active elements
+        // Element Secondary: color for content's text
         surface: "#F7F7F7",
-        "on-surface-1": "#FFFFFF",
-        "stroke-bold": "#D1D1D1",
-        "on-surface-2": "#E5E5E5",
-        "stroke-light": "#C7C7C7",
-        "brand-color": "#151515",
-        "brand-color-text": "#eee",
-        "brand-color-dark": "#0C0C0C",
-      },
+        // main background color
 
+        "on-surface-1": "#FFFFFF",
+        // background color for sections, blocks, cards
+
+        "stroke-bold": "#D1D1D1",
+        // border for section on surface 1
+
+        "on-surface-2": "#E5E5E5",
+        // highlight small elements darker than on-surface-1
+
+        "stroke-light": "#C7C7C7",
+        // border for surface
+
+        "brand-color": "#151515",
+        // color that rarely change
+
+        "element-primary": "#0A0A0A",
+        // background color
+
+        "element-secondary": "#575757",
+
+        "dark-element-primary": "#FAFAFA",
+
+        "dark-element-secondary": "#9E9E9E",
+
+      },
+      screens: {
+        'tablet': '640px',
+        // => @media (min-width: 640px) { ... }
+
+        'laptop': '1024px',
+        // => @media (min-width: 1024px) { ... }
+
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    flowbite.plugin(),
+  ],
 }
