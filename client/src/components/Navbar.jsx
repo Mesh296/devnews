104b6b94abb4c8884d3028c712821fc7876f35a4
Mesh296 <minhhan181@gmail.com>
2025-01-15 04:07:53 +0300

    feat: create sidebar, card components

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ export const Navbar = () => {
 
 
     return (
-        <nav className=" backdrop-blur-sm bg-opacity-70 bg-surface border-gray-200 dark:bg-gray-900 drop-shadow-sm sticky top-0">
+        <nav className=" backdrop-blur-sm bg-opacity-70 bg-surface border-gray-200 drop-shadow-sm sticky top-0">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { Card } from './Card'
+import { Sidebar } from '../components/Sidebar'
+import { content } from 'flowbite-react/tailwind'
 
 export const HomePage = () => {
+
   return (
-    <div>HomePage</div>
+    <div >
+      <Sidebar />
+      <div className="p-4 sm:ml-64">
+        <Card
+          title={"abc"}
+          content={"as dasda sda dasd lk; jl;k jwlqkje"}
+        />
+        <Card
+          title={"abc"}
+          content={"as dasda sda dasd lk; jl;k jwlqkje"}
+        />
+        <Card
+          title={"abc"}
+          content={"as dasda sda dasd lk; jl;k jwlqkje"}
+        />
+        <Card
+          title={"abc"}
+          content={"as dasda sda dasd lk; jl;k jwlqkje"}
+        />
+        <Card
+          title={"abc"}
+          content={"as dasda sda dasd lk; jl;k jwlqkje"}
+        />
+        <Card
+          title={"abc"}
+          content={"as dasda sda dasd lk; jl;k jwlqkje"}
+        />
+        <Card
+          title={"abc"}
+          content={"as dasda sda dasd lk; jl;k jwlqkje"}
+        />
+        <Card
+          title={"abc"}
+          content={"as dasda sda dasd lk; jl;k jwlqkje"}
+        />
+        <Card
+          title={"abc"}
+          content={"as dasda sda dasd lk; jl;k jwlqkje"}
+        />
+
+      </div>
+
+    </div>
   )
 }
