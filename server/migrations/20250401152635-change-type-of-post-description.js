c77e019f38c18885af39da556fe4e5007dce41af
Mesh296 <minhhan181@gmail.com>
2025-04-01 18:55:34 +0300

    fix: change type of description column in post table

---
+++ server/migrations/20250401152635-change-type-of-post-description.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.changeColumn('posts', 'description', {
+      type: Sequelize.TEXT,
+      allowNull: true
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.changeColumn('posts', 'description', {
+      type: Sequelize.STRING,
+      allowNull: true
+    });
+  }
+};
